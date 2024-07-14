15d4e9765ec9c2e160a42e81d87a7323771a4412
Deepak <delta12kilo@users.noreply.github.com>
2024-07-14 21:35:19 +0530

    Initial Commit

---
+++ src/test/java/com/bravuthon/skillmatrix/SkillmatrixApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bravuthon.skillmatrix;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillmatrixApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
